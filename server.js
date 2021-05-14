c85f873c61d625300e37b2aad5da0ad7c6775c38
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-14 12:47:14 -0400

    add business logic

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const { response } = require('express');
 const express = require('express');
 const path = require('path');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,7 +19,7 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use('/', routes({feedbackService, speakerService}));
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}!`);
