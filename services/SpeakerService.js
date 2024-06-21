7f3e5f43c4e32cf008a5950019408f0c4c0c111c
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 13:23:42 -0400

    Refactor and simplify SpeakerService.js
    Removed unnecessary comments and simplified code in SpeakerService.js. Abstracted repeated findSpeaker functionality into a separate method. Implemented modern JavaScript syntax for cleaner, more efficient code.

--- services/SpeakerService.js
+++ services/SpeakerService.js
@@ -1,116 +1,49 @@
 const fs = require('fs');
 const util = require('util');
-
-/**
- * We want to use async/await with fs.readFile - util.promisfy gives us that
- */
 const readFile = util.promisify(fs.readFile);
 
-/**
- * Logic for fetching speakers information
- */
 class SpeakerService {
-  /**
-   * Constructor
-   * @param {*} datafile Path to a JSOn file that contains the speakers data
-   */
   constructor(datafile) {
     this.datafile = datafile;
   }
 
-  /**
-   * Returns a list of speakers name and short name
-   */
   async getNames() {
     const data = await this.getData();
-
-    // We are using map() to transform the array we get into another one
-    return data.map((speaker) => {
-      return { name: speaker.name, shortname: speaker.shortname };
-    });
+    return data.map(({ name, shortname }) => ({ name, shortname }));
   }
 
-  /**
-   * Get all artwork
-   */
   async getAllArtwork() {
     const data = await this.getData();
-
-    // Array.reduce() is used to traverse all speakers and
-    // create an array that contains all artwork
-    const artwork = data.reduce((acc, elm) => {
-      if (elm.artwork) {
-        // eslint-disable-next-line no-param-reassign
-        acc = [...acc, ...elm.artwork];
-      }
-      return acc;
-    }, []);
-    return artwork;
+    return data.reduce((acc, { artwork }) => artwork ? [...acc, ...artwork] : acc, []);
   }
 
-  /**
-   * Get all artwork of a given speaker
-   * @param {*} shortname The speakers short name
-   */
   async getArtworkForSpeaker(shortname) {
-    const data = await this.getData();
-    const speaker = data.find((elm) => {
-      return elm.shortname === shortname;
-    });
-    if (!speaker || !speaker.artwork) return null;
-    return speaker.artwork;
+    const speaker = await this.findSpeaker(shortname);
+    return speaker?.artwork || null;
   }
 
-  /**
-   * Get speaker information provided a shortname
-   * @param {*} shortname
-   */
   async getSpeaker(shortname) {
-    const data = await this.getData();
-    const speaker = data.find((elm) => {
-      return elm.shortname === shortname;
-    });
+    const speaker = await this.findSpeaker(shortname);
     if (!speaker) return null;
-    return {
-      title: speaker.title,
-      name: speaker.name,
-      shortname: speaker.shortname,
-      description: speaker.description,
-    };
+    const { title, name, shortname: sn, description } = speaker;
+    return { title, name, shortname: sn, description };
   }
 
-  /**
-   * Returns a list of speakers with only the basic information
-   */
   async getListShort() {
     const data = await this.getData();
-    return data.map((speaker) => {
-      return {
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title,
-      };
-    });
+    return data.map(({ name, shortname, title }) => ({ name, shortname, title }));
   }
 
-  /**
-   * Get a list of speakers
-   */
   async getList() {
     const data = await this.getData();
-    return data.map((speaker) => {
-      return {
-        name: speaker.name,
-        shortname: speaker.shortname,
-        title: speaker.title,
-        summary: speaker.summary,
-      };
-    });
+    return data.map(({ name, shortname, title, summary }) => ({ name, shortname, title, summary }));
   }
 
-  /**
-   * Fetches speakers data from the JSON file provided to the constructor
-   */
+  async findSpeaker(shortname) {
+    const data = await this.getData();
+    return data.find(speaker => speaker.shortname === shortname);
+  }
+
   async getData() {
     const data = await readFile(this.datafile, 'utf8');
     return JSON.parse(data).speakers;
