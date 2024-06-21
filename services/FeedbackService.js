c53089ec69582da1cad16b04040cb99b4bb359f4
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-06-21 13:23:31 -0400

    Refactor and improve FeedbackService.js
    This commit refactors the FeedbackService class. It improves the code readability by adding more type-defined parameters and consistent variable naming conventions. It also separates the logic of saving data to a new method to promote code reusability.

--- services/FeedbackService.js
+++ services/FeedbackService.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const util = require('util');
-
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+
+const ENCODING = 'utf8';
 
 /**
  * Logic for reading and writing feedback data
@@ -10,39 +11,50 @@ const writeFile = util.promisify(fs.writeFile);
 class FeedbackService {
   /**
    * Constructor
-   * @param {*} datafile Path to a JSOn file that contains the feedback data
+   * @param {string} dataFile Path to a JSON file that contains the feedback data
    */
-  constructor(datafile) {
-    this.datafile = datafile;
+  constructor(dataFile) {
+    this.dataFile = dataFile;
   }
 
   /**
    * Get all feedback items
+   * @returns {Promise<Array>} List of feedback items
    */
   async getList() {
-    const data = await this.getData();
-    return data;
+    return this.getData();
   }
 
   /**
    * Add a new feedback item
-   * @param {*} name The name of the user
-   * @param {*} title The title of the feedback message
-   * @param {*} message The feedback message
+   * @param {string} name The name of the user
+   * @param {string} email The email of the user
+   * @param {string} title The title of the feedback message
+   * @param {string} message The feedback message
+   * @returns {Promise<void>}
    */
   async addEntry(name, email, title, message) {
-    const data = (await this.getData()) || [];
+    const data = await this.getData();
     data.unshift({ name, email, title, message });
-    return writeFile(this.datafile, JSON.stringify(data));
+    await this.saveData(data);
   }
 
   /**
    * Fetches feedback data from the JSON file provided to the constructor
+   * @returns {Promise<Array>} The feedback data
    */
   async getData() {
-    const data = await readFile(this.datafile, 'utf8');
-    if (!data) return [];
-    return JSON.parse(data);
+    const data = await readFile(this.dataFile, ENCODING);
+    return data ? JSON.parse(data) : [];
+  }
+
+  /**
+   * Saves feedback data to the JSON file
+   * @param {Array} data The feedback data
+   * @returns {Promise<void>}
+   */
+  async saveData(data) {
+    await writeFile(this.dataFile, JSON.stringify(data));
   }
 }
 
